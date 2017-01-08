0cd24d74164b088f03a4eb10cfd81b9441d8428e
Benoît Hubert <benoithubert@gmail.com>
2017-01-08 14:38:17 +0100

    Amend schema => place entry references city id

--- schema.sql
+++ schema.sql
@@ -3,7 +3,7 @@ CREATE TABLE `places` (
   -- `user_id` int(10) unsigned NOT NULL,
   `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   `street_address` varchar(120) COLLATE utf8_unicode_ci NOT NULL,
-  `city` varchar(80) COLLATE utf8_unicode_ci NOT NULL,
+  `city_id` int(10) unsigned NOT NULL,
   `postcode` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
   `state_id` int(10) unsigned NOT NULL,
   `country_id` int(10) unsigned NOT NULL,
@@ -17,6 +17,7 @@ CREATE TABLE `places` (
   -- `completed_at` timestamp NULL DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (city_id) REFERENCES cities(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
