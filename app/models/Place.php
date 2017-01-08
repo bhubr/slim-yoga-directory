16c707ab8e498b07487d636a1991ab19a88fe8bc
Benoît Hubert <benoithubert@gmail.com>
2017-01-08 14:44:17 +0100

    Ok place creation works with city_id

--- app/models/Place.php
+++ app/models/Place.php
@@ -3,9 +3,13 @@
 use Illuminate\Database\Eloquent\Model;
 
 class Place extends Model {
-  protected $fillable = ['name', 'street_address', 'city', 'postcode', 'country_id', 'state_id', 'phone', 'latitude', 'longitude', 'map_link', 'map_embed'];  
+  protected $fillable = ['name', 'street_address', 'city_id', 'postcode', 'country_id', 'state_id', 'phone', 'latitude', 'longitude', 'map_link', 'map_embed'];
 
   public function schools() {
     return $this->belongsToMany('School');
+  }
+
+  public function city() {
+    return $this->belongsTo('City');
   }
 }
